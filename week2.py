c85917a3e519e4ac64779bf0976583e143081eaf
Richard Akinwande <akinwanderichardoluwafemi@gmail.com>
2026-01-22 03:22:35 +0100

    Week 2 Day 1: Python basics

---
+++ week2.py
@@ -0,0 +1,14 @@
+# Week 2 Day 1 - Python Basics
+
+name = "Richard"
+role = "Cloud / DevOps Trainee"
+weeks_completed = 2
+
+print("Name:", name)
+print("Role:", role)
+print("Weeks completed:", weeks_completed)
+
+services = ["EC2", "S3", "IAM"]
+for service in services:
+    print("Learning AWS service:", service)
+
